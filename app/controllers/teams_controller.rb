cdc1a4efb038bf44e23b78d7fa7fa667dcd9c5c1
木村　幸介 <kosukekimura0528@gmail.com>
2019-05-08 17:37:34 +0900

    issue#65
    - 権限移譲ボタンをteamのshowビューに追加
    - team_controllerにtransferアクションを追加して、権限移譲を設定
    - showへのredirectを追加

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -51,6 +51,14 @@ class TeamsController < ApplicationController
     @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
   end
 
+  def transfer
+    team = Team.find(params[:team])
+    user_id = Assign.find(params[:assign]).user_id
+    team.owner_id = user_id
+    team.save
+    redirect_to team_path(team), notice: 'リーダーを変更しました'
+  end
+
   private
 
   def set_team
