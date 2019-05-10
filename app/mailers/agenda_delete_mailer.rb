f723f9446d8c4b2e9fbc395e32731b89e837a4b9
木村　幸介 <kosukekimura0528@gmail.com>
2019-05-10 17:05:07 +0900

    issue#67
    - emailの宛先に不足があったため、Userモデルのmembersを使用して修正

--- app/mailers/agenda_delete_mailer.rb
+++ app/mailers/agenda_delete_mailer.rb
@@ -2,7 +2,7 @@ class AgendaDeleteMailer < ApplicationMailer
   def agenda_delete_mail(team, agenda)
     @team = team
     @agenda = agenda
-    @emails = @team.users.pluck(:email)
+    @emails = @team.members.pluck(:email)
     mail to: @emails, subject: 'agendaが削除されました'
   end
 end
